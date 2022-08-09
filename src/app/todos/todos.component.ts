9b06ef3bb5c25a95df61bf9e2bcdb5c4cb37c1a9
JanicHolzherr <69592188+SomethingLikeAChicken@users.noreply.github.com>
2022-08-09 16:47:41 +0200

    Initial Commit

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../models/Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Array<Todo> = [];
+  inputTodo:string ="";
+  inputCategory:string = "";
+  todosIsEmpty = false;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'first-Todo',
+        completed: false
+      },
+      {
+        content: 'second-Todo',
+        completed: true
+      }
+    ]
+  }
+  toggleDone(id : number){
+    this.todos.map((v, i) =>{
+      if(i == id) v.completed = !v.completed;
+      return v;
+    })
+  }
+  deleteToDo(id : number){
+    this.todos = this.todos.filter((v, i) => i != id);
+  }
+  addTodo(){
+    
+    this.todos.push({
+        content: this.inputTodo,
+        completed: false
+      })
+      this.inputTodo = "";
+    
+  }
+  deleteAll(){
+    for(let i = 0; i <= this.todos.length; i++){
+      this.todos.splice(i, this.todos.length);
+    }
+  }
+  isButtonVisible(){
+    if(this.todos.length == 0)
+  {
+    this.todosIsEmpty = true;
+  }else{
+    this.todosIsEmpty = false;
+  }
+    
+  }
+}
